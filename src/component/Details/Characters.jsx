fa6115ea76ebffbb3c9995f3334b8f8d35345c87
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-09 00:57:34 +0700

    add scroller

--- src/component/Details/Characters.jsx
+++ src/component/Details/Characters.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect } from 'react'
+import { Card, Image } from 'react-bootstrap';
 import {getMovieCast} from '../../database/db'
-
+import ScrollMenu from 'react-horizontal-scrolling-menu';
+import { IoIosArrowDropleft, IoIosArrowDropright} from 'react-icons/io'
 export default function Characters() {
     const [cast, setCast] = useState([]);
     const [crew, setCrew] = useState([]);
     const [isLoading, setLoading] = useState(false)
     const [isError, setError] = useState(false)
+
+    const ArrowLeft = () => {
+        return <IoIosArrowDropleft/>
+    }
+    
+    const ArrowRight = () => {
+        return <IoIosArrowDropright/>
+    }
 
     useEffect(() => {
         const fetchData = async () => {
@@ -25,13 +35,16 @@ export default function Characters() {
 
         fetchData()
     }, [])
-
+                //cast.map(item=> (<p key={item.id}>{item.character}</p>))
     return (
         <section className="credits">
             <div className="credits--cast">
-                {isLoading && <p>LOADING NOW DUDEK</p>}
-                {console.log(cast)}
-                {cast.length && cast.map(item=> (<p>{item.character}</p>))}
+                {isError && <div>Something went wrong ...</div>}
+                {isLoading ? <p>LOADING NOW DUDEK</p> : cast.length && 
+                    
+                    <ScrollMenu data={cast}/>
+                }
+
             </div>
         </section>
     )
