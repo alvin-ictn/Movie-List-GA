36fcda6caa9aba5b52a612efbd9bd5cc2ba6619b
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-09 10:48:19 +0700

    fix horizontal slider

--- src/component/Details/Characters.jsx
+++ src/component/Details/Characters.jsx
@@ -1,51 +1,88 @@
-import React, { useState, useEffect } from 'react'
-import { Card, Image } from 'react-bootstrap';
-import {getMovieCast} from '../../database/db'
-import ScrollMenu from 'react-horizontal-scrolling-menu';
-import { IoIosArrowDropleft, IoIosArrowDropright} from 'react-icons/io'
+import React, { useState, useEffect } from "react";
+import { Card, Image } from "react-bootstrap";
+import { getMovieCast } from "../../database/db";
+import ScrollMenu from "react-horizontal-scrolling-menu";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+
 export default function Characters() {
-    const [cast, setCast] = useState([]);
-    const [crew, setCrew] = useState([]);
-    const [isLoading, setLoading] = useState(false)
-    const [isError, setError] = useState(false)
+  const [cast, setCast] = useState([]);
+  const [crew, setCrew] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
 
-    const ArrowLeft = () => {
-        return <IoIosArrowDropleft/>
-    }
-    
-    const ArrowRight = () => {
-        return <IoIosArrowDropright/>
-    }
+  const [list, setList] = useState([]);
+  const [selected, setSelected] = useState(1)
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setError(false)
-            setLoading(true)
-            
-            try{
-                const result = await getMovieCast()
-                setCast(result.cast)
-                setCrew(result.crew)
-            } catch(error){
-                setError(true)
-            }
-
-            setLoading(false)
-        }
-
-        fetchData()
-    }, [])
-                //cast.map(item=> (<p key={item.id}>{item.character}</p>))
+  const MenuItem = ({ text, selected }) => {
+    return <div className={`menu-item ${selected ? 'active' : ''}`}>{text}</div>;
+  };
+  
+  const Menu = (list, selected) =>
+  list.map(el => {
+    const { character, cast_id } = el;
+    const onClick = () => console.log('original onClick ', character);
     return (
-        <section className="credits">
-            <div className="credits--cast">
-                {isError && <div>Something went wrong ...</div>}
-                {isLoading ? <p>LOADING NOW DUDEK</p> : cast.length && 
-                    
-                    <ScrollMenu data={cast}/>
-                }
+      <MenuItem text={character} key={cast_id} selected={selected} onClick={onClick} />
+    );
+  });
 
+  const ArrowLeft = () => {
+    return <IoIosArrowDropleft />;
+  };
+
+  const ArrowRight = () => {
+    return <IoIosArrowDropright />;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false);
+      setLoading(true);
+
+      try {
+        const result = await getMovieCast();
+        setCast(result.cast);
+        setCrew(result.crew);
+      } catch (error) {
+        setError(true);
+      }
+
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+      console.log(cast)
+    let data = cast.map((item) => <div key={item.cast_id}>{item.character}</div>);
+    setList(data);
+    console.log(data)
+  }, [cast]);
+
+  const onSelect = key => {
+
+  }
+  //cast.map(item=> (<p key={item.id}>{item.character}</p>))
+  return (
+    <section className="credits">
+      <div className="credits--cast">
+        {isError && <div>Something went wrong ...</div>}
+        {isLoading ? (
+          <p>LOADING NOW DUDEK</p>
+        ) : (
+          cast.length && (
+            <div>
+                {list.map(item=>item)}
+              {/* <ScrollMenu
+                data={list}
+                arrowLeft={ArrowLeft}
+                arrowRight={ArrowRight}
+              /> */}
             </div>
-        </section>
-    )
+          )
+        )}
+      </div>
+    </section>
+  );
 }
